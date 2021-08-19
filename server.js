a1c9e97bf060ed2dae89c559b26fb5efc20fb50a
Mahmoud Fahiem <xmahmoud92@gmail.com>
2021-08-19 18:02:23 +0200

    feat: Config express app

---
+++ server.js
@@ -0,0 +1,29 @@
+// Setup empty JS object to act as endpoint for all routes
+projectData = {};
+
+// Require Express to run server and routes
+const express = require('express');
+
+// Start up an instance of app
+const app = express();
+
+/* Middleware*/
+//Here we are configuring express to use body-parser as middle-ware.
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// Cors for cross origin allowance
+const cors = require('cors');
+app.use(cors());
+
+// Initialize the main project folder
+app.use(express.static('public'));
+
+
+// Setup Server
+const port = 8000;
+const listening = () => {
+  console.log('Server is running....');
+  console.log(`Running on port ${port}`)
+}
+app.listen(port, listening);
